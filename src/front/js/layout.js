b99c6c55283e424382af03b0c802b2292ff2c2e6
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-06-26 17:35:24 +0000

    esbozado recuperar contraseña

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Recover } from "./pages/recover";
 import { Login } from "./pages/login";
 
 import injectContext from "./store/appContext";
@@ -45,6 +46,7 @@ const Layout = () => {
                                 <Route element={<Demo />} path="/demo" />
                                 <Route element={<Login />} path="/login" />
                                 <Route element={<Signup />} path="/signup" />
+                                <Route element={<Recover />} path="/recover" />
                                 <Route element={<Single />} path="/single/:theid" />
                                 <Route element={<h1>Not found!</h1>} />
                             </Routes>
--- src/front/js/pages/recover.js
+++ src/front/js/pages/recover.js
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
-export const Login = () => {
+
+
+
+export const Recover = () => {
     const { store, actions } = useContext(Context);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -42,51 +45,32 @@ export const Login = () => {
     return (
         <div className="container text-center justify-content-center">
             <br />
-            <h1>Acceso de Usuario</h1>
+            <h1>Recuperar contraseña</h1>
             <br />
             <br />
             <div>
                 <div className="container wrapper d-flex justify-content-center">
                     <form onSubmit={handleSubmit}>
                         <div className="form-group">
-                            <label htmlFor="exampleInputEmail1" className="h4">Dirección de Correo</label>
+                            <label htmlFor="exampleInputEmail1" className="h4">Introduce el correo de recuperación </label>
                             <input
                                 type="email"
                                 className="form-control"
                                 id="exampleInputEmail1"
                                 aria-describedby="emailHelp"
-                                placeholder="Tu correo"
+                                placeholder="Correo al que enviar tu clave de recuperación"
                                 value={email}
                                 onChange={handleEmailChange}
                             />
                         </div>
-                        <br />
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1" className="h4">Contraseña</label>
-                            <div className="d-flex">
-                            <input
-                                type={eye == false ? "text" : "password"}
-                                className="form-control me-3"
-                                id="exampleInputPassword1"
-                                placeholder="Contraseña"
-                                value={password}
-                                onChange={handlePasswordChange}
-                            />
-                            <i className={eye == true ? "fa-solid fa-eye-slash m-auto" : "fa-solid fa-eye m-auto"} onClick={handleEye}></i>
-                            </div>
-                            <small>¿No recuerdas tu contraseña? <Link to="recover">Recuperar aquí</Link></small>
-                        </div>
-                        <br />
-                        <button type="submit" className="btn btn-primary">Enviar</button>
+                      
                     </form>
                 </div>
                 <br />
                 <br />
                 <br />
                 <br />
-                <div className="container d-flex justify-content-center">
-                    <h5>Si no te has registrado, puedes hacerlo <Link to="/login">Aquí</Link></h5>
-                </div>
+                
             </div>
         </div>
     );
