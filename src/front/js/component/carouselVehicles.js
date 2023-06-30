fc0cca29f7e94dc30323e3027ca1a38a9549e19f
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-06-30 16:43:01 +0000

    falta arreglar responsive

--- src/front/js/component/carouselVehicles.js
+++ src/front/js/component/carouselVehicles.js
@@ -2,6 +2,7 @@ import React, {useContext} from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { ThemeContext } from "../layout";
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -9,7 +10,8 @@ export const CarouselVehicles = () => {
     
     const {store, actions} = useContext(Context)
     const carImage = "https://images.coches.com/_vn_/kia/Sportage/c399cf1d98a95d24f8e8715dd0b13fb2.jpg?p=cc_vn_high"
-    
+    const navigate = useNavigate()
+
     return (
       <div className="container">
           <div className="row pb-4 my-4">
@@ -19,7 +21,22 @@ export const CarouselVehicles = () => {
             
                 <div className="col-12 col-md-4">
                   <div className="card" style={{width: "18rem"}}>
-                    <img src={carImage} className="card-img-top" alt="..."/>
+                  <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <img src={carImage} className="card-img-top imgCarousel" alt="..."/>
+                    </div>
+                    <div class="flip-card-back">
+                      <Link to="/login" style={{ color: 'white', textDecoration: 'none' }} className="link-hover">
+                      <h3 className="pt-2">Seat Ibiza</h3> 
+                      </Link>
+                      <p>Matriculación: 2017</p> 
+                      <p>Cambio: Manual</p>
+                      <p>120.000 km</p>
+                      <p>Ubicación: Córdoba</p>
+                    </div>
+                  </div>
+                </div>
                     <div className="d-flex justify-content-end m-2">
                       <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
                       <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
@@ -27,7 +44,9 @@ export const CarouselVehicles = () => {
                     </div>
                     <div className="card-body d-flex justify-content-between">
                       <div>
+                        <Link to="/login" style={{ color: 'black', textDecoration: 'none' }} className="link-hover">
                         <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">Coche fantástico</h5>
+                        </Link>
                         <h5 className="card-title justify-content-start d-flex">20.000€</h5>
                       </div>
 
@@ -46,12 +65,270 @@ export const CarouselVehicles = () => {
                     </div>
 
                   </div>
+                  
+                </div>
+                {
+  /* Segundo coche de ejemplo */
+}
+           <div className="col-12 col-md-4">
+                  <div className="card" style={{width: "18rem"}}>
+                  <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <img src={carImage} className="card-img-top" alt="..."/>
+                    </div>
+                    <div class="flip-card-back">
+                      <Link to="/login" style={{ color: 'white', textDecoration: 'none' }} className="link-hover">
+                      <h3 className="pt-2">Peugeot 507</h3> 
+                      </Link>
+                      <p>Matriculación: 2020</p> 
+                      <p>Cambio: Automático</p>
+                      <p>180.000 km</p>
+                      <p>Ubicación: Migard</p>
+                    </div>
+                  </div>
+                </div>
+                    <div className="d-flex justify-content-end m-2">
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                    </div>
+                    <div className="card-body d-flex justify-content-between">
+                      <div>
+                          <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">
+                          <Link to="/login" style={{ color: 'black', textDecoration: 'none' }}>
+                            Coche fantástico
+                          </Link> 
+                          </h5>
+                        <h5 className="card-title justify-content-start d-flex">20.000€</h5>
+                      </div>
+
+                      <div className="d-flex justify-content-end">                
+                     
+                      <Link
+                        id="heartCard"
+                        href="#"
+                        role="button"
+                        aria-expanded="page"
+                        to="/profile"
+                      >
+                        <i className="fa-regular fa-heart"></i>
+                      </Link>
+                      </div>
+                    </div>
+
+                  </div>
+                  
                 </div>
 
-        
-            </div>
-            </div>
 
+                <div className="col-12 col-md-4">
+                  <div className="card" style={{width: "18rem"}}>
+                  <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <img src={carImage} className="card-img-top" alt="..."/>
+                    </div>
+                    <div class="flip-card-back">
+                      <Link to="/login" style={{ color: 'white', textDecoration: 'none' }} className="link-hover">
+                      <h3 className="pt-2">Peugeot 507</h3> 
+                      </Link>
+                      <p>Matriculación: 2020</p> 
+                      <p>Cambio: Automático</p>
+                      <p>180.000 km</p>
+                      <p>Ubicación: Migard</p>
+                    </div>
+                  </div>
+                </div>
+                    <div className="d-flex justify-content-end m-2">
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                    </div>
+                    <div className="card-body d-flex justify-content-between">
+                      <div>
+                          <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">
+                          <Link to="/login" style={{ color: 'black', textDecoration: 'none' }}>
+                            Coche fantástico
+                          </Link> 
+                          </h5>
+                        <h5 className="card-title justify-content-start d-flex">20.000€</h5>
+                      </div>
+
+                      <div className="d-flex justify-content-end">                
+                     
+                      <Link
+                        id="heartCard"
+                        href="#"
+                        role="button"
+                        aria-expanded="page"
+                        to="/profile"
+                      >
+                        <i className="fa-regular fa-heart"></i>
+                      </Link>
+                      </div>
+                    </div>
+
+                  </div>
+                  
+                </div>
+                <div className="col-12 col-md-4">
+                  <div className="card" style={{width: "18rem"}}>
+                  <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <img src={carImage} className="card-img-top" alt="..."/>
+                    </div>
+                    <div class="flip-card-back">
+                      <Link to="/login" style={{ color: 'white', textDecoration: 'none' }} className="link-hover">
+                      <h3 className="pt-2">Peugeot 507</h3> 
+                      </Link>
+                      <p>Matriculación: 2020</p> 
+                      <p>Cambio: Automático</p>
+                      <p>180.000 km</p>
+                      <p>Ubicación: Migard</p>
+                    </div>
+                  </div>
+                </div>
+                    <div className="d-flex justify-content-end m-2">
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                    </div>
+                    <div className="card-body d-flex justify-content-between">
+                      <div>
+                          <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">
+                          <Link to="/login" style={{ color: 'black', textDecoration: 'none' }}>
+                            Coche fantástico
+                          </Link> 
+                          </h5>
+                        <h5 className="card-title justify-content-start d-flex">20.000€</h5>
+                      </div>
+
+                      <div className="d-flex justify-content-end">                
+                     
+                      <Link
+                        id="heartCard"
+                        href="#"
+                        role="button"
+                        aria-expanded="page"
+                        to="/profile"
+                      >
+                        <i className="fa-regular fa-heart"></i>
+                      </Link>
+                      </div>
+                    </div>
+
+                  </div>
+                  
+                </div>
+                <div className="col-12 col-md-4">
+                  <div className="card" style={{width: "18rem"}}>
+                  <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <img src={carImage} className="card-img-top" alt="..."/>
+                    </div>
+                    <div class="flip-card-back">
+                      <Link to="/login" style={{ color: 'white', textDecoration: 'none' }} className="link-hover">
+                      <h3 className="pt-2">Peugeot 507</h3> 
+                      </Link>
+                      <p>Matriculación: 2020</p> 
+                      <p>Cambio: Automático</p>
+                      <p>180.000 km</p>
+                      <p>Ubicación: Migard</p>
+                    </div>
+                  </div>
+                </div>
+                    <div className="d-flex justify-content-end m-2">
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                    </div>
+                    <div className="card-body d-flex justify-content-between">
+                      <div>
+                          <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">
+                          <Link to="/login" style={{ color: 'black', textDecoration: 'none' }}>
+                            Coche fantástico
+                          </Link> 
+                          </h5>
+                        <h5 className="card-title justify-content-start d-flex">20.000€</h5>
+                      </div>
+
+                      <div className="d-flex justify-content-end">                
+                     
+                      <Link
+                        id="heartCard"
+                        href="#"
+                        role="button"
+                        aria-expanded="page"
+                        to="/profile"
+                      >
+                        <i className="fa-regular fa-heart"></i>
+                      </Link>
+                      </div>
+                    </div>
+
+                  </div>
+                  
+                </div>
+                <div className="col-12 col-md-4">
+                  <div className="card" style={{width: "18rem"}}>
+                  <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <img src={carImage} className="card-img-top" alt="..."/>
+                    </div>
+                    <div class="flip-card-back">
+                      <Link to="/login" style={{ color: 'white', textDecoration: 'none' }} className="link-hover">
+                      <h3 className="pt-2">Peugeot 507</h3> 
+                      </Link>
+                      <p>Matriculación: 2020</p> 
+                      <p>Cambio: Automático</p>
+                      <p>180.000 km</p>
+                      <p>Ubicación: Migard</p>
+                    </div>
+                  </div>
+                </div>
+                    <div className="d-flex justify-content-end m-2">
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                      <i class="fa-solid fa-star" style={{"color": "#bbc615"}}></i>
+                    </div>
+                    <div className="card-body d-flex justify-content-between">
+                      <div>
+                          <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">
+                          <Link to="/login" style={{ color: 'black', textDecoration: 'none' }}>
+                            Coche fantástico
+                          </Link> 
+                          </h5>
+                        <h5 className="card-title justify-content-start d-flex">20.000€</h5>
+                      </div>
+
+                      <div className="d-flex justify-content-end">                
+                     
+                      <Link
+                        id="heartCard"
+                        href="#"
+                        role="button"
+                        aria-expanded="page"
+                        to="/profile"
+                      >
+                        <i className="fa-regular fa-heart"></i>
+                      </Link>
+                      </div>
+                    </div>
+
+                  </div>
+                  
+                </div>
+                {
+  /* A partir de aquí se ponen más, justo encima*/
+}
+            </div>
+            </div>
+            
+            
             <br></br>
             <br></br>
             <br></br>
--- src/front/js/component/garageAdvice.js
+++ src/front/js/component/garageAdvice.js
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import { useNavigate } from "react-router-dom";
+
 import { Context } from "../store/appContext";
 import { ThemeContext } from "../layout";
 import "../../styles/index.css";
@@ -9,65 +9,56 @@ import "../../styles/index.css";
 export const GarageAdvice = () => {
     
     const {store, actions} = useContext(Context)
-    const garageImage = "https://a-solis.com/wp-content/uploads/2020/04/taller-mecanico-malaga.jpg"
+    const garageImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFOZ2L1ab7nVYrVxuxVUXMBSokaf-6FWNvug&usqp=CAU"
     
-    const navigate = useNavigate()
+
 
     return (
     
-      <div className="container mt-5 d-flex mx-auto d-flex justify-content-between " >
+      <div className="container mt-5 d-flex mx-auto d-flex justify-content-between  flex-wrap my-3 flex-xs-column flex-ms-column" >
 
 
-         <div className="container col-5 mx-4  col-sm-12 col-md-5  container-garage">
-         
-
+         <div className="container col-5 mx-2  col-sm-12 col-xs-12 col-md-5 d-md-block d-sm-block d-xs-block container-garage">
+          
                     <img src={garageImage} className="card-img garageImageLanding d-none d-sm-block d-sm-none d-md-block" alt="..."/>
-                    <h5 className="text-end pt-2 pe-3">Consulta nuestros Talleres</h5>            
-                    <p className="text-end pt-2 px-3 textGarageLeft d-flex"> 
+                    
+                    
+                    <h5 className="text-end pt-3 pe-3">Consulta nuestros Talleres</h5>            
+                    <p className="text-end pt-2 px-3 textGarageLeft d-flex" style={{"font-size": "larger"}}> 
                     Tasamos vuestros vehículos como garantía su estado. Las ventas serán más rápidas y las compras serán más seguras así.
                     </p>
                    
-
+                    <div className="mt-5 justify-content-end justify-content-bottom">
                     <button className="btn-plus btn-mas m-2 float-end buttonGarage">
                         <i className="fa-solid fa-plus"></i>
                     </button>
+                    </div>
                    
                </div>
          
 
 
-            <div className="wrapper col-4 mx-4   col-sm-12 col-md-5 justify-content-between container-garage">
+            <div className="wrapper flex-wrap  col-5 mx-2  d-md-block d-sm-block d-xs-block col-sm-12 col-md-5 justify-content-between container-garage">
            
-            <h5 className="text-end pt-2 pe-3">¿Eres un Taller?</h5>            
-                    <p className="text-start pt-2 px-3 textGarageRight d-flex"> 
+            <h5 className="text-start pt-3 px-3 mx-3">¿Eres un Taller?</h5>            
+                    <p className="text-start pt-2 px-3 mx-3 textGarageRight d-flex mt-5" style={{"font-size": "larger"}}> 
                     Date de alta y ayuda a vendedores y compradores a certificar el estado del vehículo.
                     </p>
                    
-
-                    <button className="btn-plus btn-mas m-2 float-end buttonGarage">
+                    <div className="mt-5 justify-content-end justify-content-bottom">
+                    <button className="btn-plus btn-mas m-2 float-end buttonGarage  ">
                         <i className="fa-solid fa-plus"></i>
                     </button>
+                    </div>
             
             </div>
                
 
+
+
+
         </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                
    
     
     );
