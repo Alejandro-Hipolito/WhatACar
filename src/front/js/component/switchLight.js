07aa10a1b7aeb21fe0e26b97e15c9ce90a3b7c7e
Alejandro Hipólito Hernando <126016967+Alejandro-Hipolito@users.noreply.github.com>
2023-10-28 08:44:38 +0000

    adición de alerta en el switch

--- src/front/js/component/switchLight.js
+++ src/front/js/component/switchLight.js
@@ -1,32 +1,45 @@
-
-import React, { useContext, useState, useEffect } from "react";
-
+import React, { useContext } from "react";
 import ReactSwitch from "react-switch";
 import { ThemeContext } from "../layout";
-import { Context } from "../store/appContext";
+import Swal from "sweetalert2";
 
-
-
-
-
+const darkAlert = (theme) => {
+  if (theme === "light") {
+    Swal.fire({
+      icon: 'info',
+      title: 'Esta opción está en desarrollo',
+      text: 'El modo oscuro aún no está optimizado, disculpen las molestias ocasionadas.',
+    })
+  }
+};
 
 export const SwitchLight = () => {
-    const { theme, toggleTheme } = useContext(ThemeContext);
-    return (
-      <div className="switch ms-4 mt-2 me-3">
-                <label className="mode me-2 switch">{theme === "light" 
-                ? 
-                <i className="fa-regular fa-lightbulb"/>
-                : 
-                <i className="fa-solid fa-lightbulb"/>}</label>
-                <ReactSwitch
-                  onChange={toggleTheme}
-                  checked={theme === "dark"}
-                  className="switch"
-                  checkedIcon={null}
-                  uncheckedIcon={null}
-                  onColor="#200000"
-                />
-              </div>
-    )
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  const handleToggleTheme = () => {
+    toggleTheme();
+    darkAlert(theme); 
   };
+
+  return (
+    <div className="switch ms-4 mt-2 me-3">
+      <label className="mode me-2 switch">
+        {theme === "light" ? (
+          <i className="fa-regular fa-lightbulb" />
+        ) : (
+          <i className="fa-solid fa-lightbulb" />
+        )}
+      </label>
+      <ReactSwitch
+        onChange={handleToggleTheme}
+        checked={theme === "dark"}
+        className="switch"
+        checkedIcon={null}
+        uncheckedIcon={null}
+        onColor="#200000"
+        
+        
+      />
+    </div>
+  );
+};
